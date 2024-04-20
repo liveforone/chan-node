a683d3e100e0c005dfb135ae2b7f28a957945600
liveforone <yc4852@gmail.com>
2024-04-20 19:00:19 +0900

    Refactor redis ttl

--- chan-nest/src/redis/constant/redis.constant.ts
+++ chan-nest/src/redis/constant/redis.constant.ts
@@ -1,3 +1,3 @@
 export const REDIS_CLIENT = 'REDIS_CLIENT';
-export const REDIS_GLOBAL_TTL = 300;
-export const REDIS_REFRESH_TOKEN_TTL = 7200;
+export const REDIS_GLOBAL_TTL = 1800; //30분
+export const REDIS_REFRESH_TOKEN_TTL = 2592000; //30일
