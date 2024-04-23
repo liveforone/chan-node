4d5311a4ae8e79db68371d4727817c5ba911ae16
liveforone <yc4852@gmail.com>
2024-04-23 15:04:29 +0900

    Refactor all api divide into two section. first one is server, second one is clinet

---
+++ chan-react/src/api/users-client.api.ts
@@ -0,0 +1,4 @@
+export const UsersClientApi = {
+  LOGIN: 'http://localhost:3000/users/login',
+  PROFILE: 'http://localhost:3000/users/profile',
+} as const;
---
+++ chan-react/src/api/users-server.api.ts
@@ -0,0 +1,10 @@
+export const UsersServerApi = {
+  SIGNUP: 'http://localhost:8080/users/signup',
+  LOGIN: 'http://localhost:8080/auth/login',
+  LOGOUT: 'http://localhost:8080/auth/logout',
+  REISSUE: 'http://localhost:8080/auth/reissue',
+  UPDATE_PASSWORD: 'http://localhost:8080/users/update/password',
+  WITHDRAW: 'http://localhost:8080/users/withdraw',
+  PROFILE: 'http://localhost:8080/users/profile',
+  USER_INFO: 'http://localhost:8080/users/info', //id param
+} as const;
