611bc283fc6ec62faee706429b00499d720ebb06
liveforone <yc4852@gmail.com>
2024-05-31 11:47:24 +0900

    Add auth exc msg when extract jwt token in header

--- chan-nest/src/global/exception/exceptionMessage/AuthExcMsg.ts
+++ chan-nest/src/global/exception/exceptionMessage/AuthExcMsg.ts
@@ -1,4 +1,7 @@
 export enum AuthExcMsg {
+  INVALID_TOKEN = 'Invalid token',
+  HEADER_NOT_FOUND = 'Authorization header not found',
+  TOKEN_NOT_FOUND = 'Jwt Token not found',
   REFRESH_TOKEN_IS_NOT_MATCH = 'Refresh 토큰이 일치하지 않습니다.',
   REFRESH_TOKEN_IS_EXPIRE = 'Refresh 토큰이 만료되었습니다. 재로그인 해주세요.',
 }
